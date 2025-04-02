59e0d01e7423f8487ae7ec7d111b2481cca4c90c
Gameywood <shettymithesh4@gmail.com>
2025-04-02 13:26:47 +0530

    Auth controller

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,20 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import  connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from "cookie-parser";
 
 
 dotenv.config(); 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true})); // to parse form data (urlencoded)
 
+app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 
 app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`); 
-    connectMongoDB()
-    ;
-}) 
+    console.log(`Server is running on port ${PORT}`); 
+    connectMongoDB();
+});
