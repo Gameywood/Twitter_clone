bc15d4b1e4adbd760b39891576d6d1a5a69cb62c
Gameywood <shettymithesh4@gmail.com>
2025-04-05 10:27:37 +0530

    Post routes & Controllers

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
     ],
     following: [
         {
-            type: mongoose.Schema.Types.ObjectId, 
+            type: mongoose.Schema.Types.ObjectId,  
             ref:"User",
             default:[]
         }
@@ -51,6 +51,13 @@ const userSchema = new mongoose.Schema({
         type:String,
         default:"",
     },
+    likedPosts:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Post",
+            default:[]
+        }
+    ]
 
 
 },{timestamp:true})
